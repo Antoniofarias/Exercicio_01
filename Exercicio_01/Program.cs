c8036685b1952ae4675d425f5836c5c5506cda01
Antonio <antoniofjj@hotmail.com>
2019-02-12 10:51:35 -0200

    Desenvolva um algoritmo que solicite a entrada de um número e calcule se o número é par ou impar.

--- Exercicio_01/Program.cs
+++ Exercicio_01/Program.cs
@@ -10,6 +10,21 @@ namespace Exercicio_01
     {
         static void Main(string[] args)
         {
+            //Desenvolva um algoritmo que solicite a entrada de um número e calcule se o número é par ou impar.
+            int n;
+
+            Console.Write("Digite o número: "); //Imprime um valor na tela
+            n = Convert.ToInt32(Console.ReadLine()); //Lê e converte para int o número informado pelo usuário
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("É par");
+            }
+            else
+            {
+                Console.WriteLine("É impar");
+            }
+            Console.ReadKey();
         }
     }
 }
